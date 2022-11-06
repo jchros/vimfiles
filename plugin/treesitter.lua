f39c29e70f8975d74a4ea6aad11bc7bd1a7522c2
Jason Chrosrova <jason.chrosrova@icloud.com>
2022-11-06 16:50:06 +0100

    Disable tree-sitter highlighting on large files

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -4,6 +4,13 @@ vim.cmd.packadd 'playground'
 require('nvim-treesitter.configs').setup {
 	highlight = { enable = true,
 		additional_vim_regex_highlighting = false,
+		disable = function (lang, buf)
+			local max_filesize = 300 * 1000 -- 300 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 	},
 	incremental_selection = { enable = true,
 		keymaps = {
