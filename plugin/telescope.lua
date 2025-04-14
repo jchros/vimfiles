56c0f4711d9d6cfa9fdd9424157f0bfb635f96f2
Jason Chrosrova <jason.chrosrova@icloud.com>
2025-04-14 22:12:32 +0200

    Add a mapping to search in Git repo

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,2 +1,14 @@
 vim.cmd.packadd 'telescope.nvim'
 vim.g.pear_tree_ft_disabled = { 'TelescopePrompt' }
+
+vim.api.nvim_set_keymap('n', '<leader>f', '', {
+	callback = function ()
+		local builtin = require 'telescope.builtin'
+		-- If the current buffer belongs in a Git repo, then search
+		-- through the files of that repo.  Otherwise, look in the repo
+		-- of the current working directory.
+		builtin.git_files {
+			use_file_path = vim.fn.finddir('.git', vim.fn.expand('%:p:h') .. ';') ~= ''
+		}
+	end;
+})
